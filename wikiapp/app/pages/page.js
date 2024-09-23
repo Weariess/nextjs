ad978a7873fbd35036c4e8b654262f013c46a97b
Weariess <125036889+Weariess@users.noreply.github.com>
2024-09-23 17:15:36 +0200

    yeehaaw

--- wikiapp/app/pages/page.js
+++ wikiapp/app/pages/page.js
@@ -1,6 +1,10 @@
 import ItemList from "/app/components/ItemList.js";
 import BlokTxt from "/app/components/BlokTxt.js";
 import ImageWithText from "/app/components/ImageWithText.js";
+import ConditionalDisplay from "/app/components/ConditionalDisplay.js"
+import SimpleForm from "/app/components/SimpleForm.js"
+import ListWithObjects from "/app/components/ListWithObjects.js"
+
 function HomePage() {
     return( <div style={{margin: "30px"}}>
     <h2 style={{margin:'15px'}}>Wiktoria Duciak</h2>
@@ -11,9 +15,21 @@ function HomePage() {
     heading="Heading"
     content="content"
     />
+
     <ImageWithText
     imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/225px-React_Logo_SVG.svg.png"
     text="Obraz"/>
+
+    <SimpleForm
+    placeholder="Wpisz tekst"/>
+
+    <ConditionalDisplay
+    isVisible="true"/>
+
+    <ListWithObjects
+    items={[{name:'test1', value:'test1'}, {name:'test2', value:'tescik'}]}
+    />
+
     </div>
     );
   }
